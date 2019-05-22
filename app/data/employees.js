f6b17f1a2b980d78574ec25f289879afcfe1427d
David Gallegos <david.gallegos@outlook.com>
2019-05-21 22:11:50 -0700

    working on express connection and first route

---
+++ app/data/employees.js
@@ -0,0 +1,33 @@
+var employees = {
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+  {
+    "name":"David Gallegos",
+    "photo":"https://unsplash.com/photos/mpw37yXc_WQ",
+    "scores":[
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+      ]
+  }
+  module.exports = employees
