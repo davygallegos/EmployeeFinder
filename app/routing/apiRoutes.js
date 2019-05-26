19593b9dea3887cfef58747a637c78edf908ce62
David Gallegos <david.gallegos@outlook.com>
2019-05-25 20:05:38 -0700

    routes config along with logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -21,29 +21,51 @@ app.get("/api/employees.js", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = [];
-    waitListData.length = [];
-
-    res.json({ ok: true });
-  });
-};
-
-//for loop within a forloop.  get total difference between my input comparing to all the available users.  I'm 1 we are comparing with all the rest
-
+    var bestMatch = {
+        name: "",
+        photo: "",
+        employeeDifference: Infinity
+      };
+  
+      // Here we take the result of the user"s survey POST and parse it.
+      var userData = req.body;
+      var userScores = userData.scores;
+  
+      // This variable will calculate the difference between the user"s scores and the scores of
+      // each user in the database
+      var totalDifference;
+  
+      // Here we loop through all the employee possibilities in the database.
+      for (var i = 0; i < employees.length; i++) {
+        var currentEmployee = employees[i];
+        totalDifference = 0;
+  
+        console.log(currentEmployee.name);
+  
+        // We then loop through all the scores of each employee
+        for (var j = 0; j < currentEmployee.scores.length; j++) {
+          var currentEmployeeScore = currentEmployee.scores[j];
+          var currentUserScore = userScores[j];
+  
+          // We calculate the difference between the scores and sum them into the totalDifference
+          totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentEmployeeScore));
+        }
+  
+        // If the sum of differences is less then the differences of the current "best match"
+        if (totalDifference <= bestMatch.employeeDifference) {
+          // Reset the bestMatch to be the new employee.
+          bestMatch.name = currentEmployee.name;
+          bestMatch.photo = currentEmployee.photo;
+          bestMatch.employeeDifference = totalDifference;
+        }
+      }
+  
+      // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+      // the database will always return that the user is the user's best employee).
+      employees.push(userData);
+  
+      // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+      res.json(bestMatch);
+    });
+  };
   
