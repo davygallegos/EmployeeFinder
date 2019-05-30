009aa52fd7396d32fb84f552c31da5c60ca7e620
David Gallegos <david.gallegos@outlook.com>
2019-05-29 18:23:25 -0700

    updated a var

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,23 +4,11 @@ var employees = require("../data/employees");
 
 
 module.exports = function (app){
-// Basic route that sends the user first to the AJAX Page
 app.get("/api/employees", function(req, res) {
     return res.json(employees);
   });
-//res.sendFile sends HTML file not JS!
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   app.post("/api/employees", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
     var bestMatch = {
         name: "",
         photo: "",
